4984220c09a0edba7fadfa133bc3b15404bb915a
Marquet Reid <marquet@mdrsoftware.com>
2026-09-22 22:31:46 +0000

    updated styles and css for learning

--- script.js
+++ script.js
@@ -5,21 +5,18 @@ const resetButton = document.querySelector("#resetButton");
 
 let size = 1;
 
-function displaySize() {
-  antMan.style.transform = `translateX(-50%) scale(${size})`;
-}
+// create a function displaySize below with the following:
+// antMan.style.transform = `translateX(-50%) scale(${size})`;
 
 shrinkButton.addEventListener("click", function () {
   size = Math.max(0.1, size - 0.1);
   displaySize();
 });
 
-growButton.addEventListener("click", function () {
-  size = Math.min(2, size + 0.1);
-  displaySize();
-});
+// Add an event listner for the grow button with the following:
+// size = Math.min(2, size + 0.1);
+//  displaySize();
 
-resetButton.addEventListener("click", function () {
-  size = 1;
-  displaySize();
-});
+// Add an event listner for the reset button with the following:
+// size = 1;
+//  displaySize();
